42eda5b8c307e92e778617e65d5764e31bba13b3
Giuliano Vallone <giuliano.vallone@ikmail.com>
2025-05-13 12:12:22 +0200

    Fixed some errors

--- ansible/roles/colosseum/terraform/files/colosseum-infrastructure/modules/networks/main.tf
+++ ansible/roles/colosseum/terraform/files/colosseum-infrastructure/modules/networks/main.tf
@@ -3,7 +3,7 @@ resource "incus_network" "colosseum-network" {
   type = var.network_type
 
   config = {
-    "ipv4.address" = "10.0.0.0/8"
+    "ipv4.address" = "10.254.0.1/8"
     "ipv4.nat" = "true"
     "ipv4.dhcp" = "true"
     "network" = "incusbr0"
@@ -13,14 +13,14 @@ resource "incus_network" "colosseum-network" {
 resource "incus_network_forward" "wireguard-forward" {
   count = length(var.teams)
 
-  network = incus_network.colosseum-network
+  network = incus_network.colosseum-network.name
   listen_address = var.cluster_address
 
   ports = [
     {
       description = "Wireguard"
       protocol = "udp"
-      listen_port = "51820+count.index"
+      listen_port = "${51820+count.index}"
       target_address = "10.80.${count.index}.254"
       target_port = "51820"
     }
