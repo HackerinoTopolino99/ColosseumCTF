a8c02ec6d880edd80f0907ae69b1668b07d31ac1
Giuliano Vallone <giuliano.vallone@ikmail.com>
2025-05-27 10:58:17 +0200

    Added profile

---
+++ ansible/roles/colosseum/terraform/files/colosseum-infrastructure/modules/profile/main.tf
@@ -0,0 +1,35 @@
+resource "incus_profile" "colosseum-profile" {
+  name = "colosseum-profile"
+
+  device {
+    type = "disk"
+    name = "root"
+
+    properties = {
+      pool = "colosseum-pool"
+      path = "/"
+    }
+  }
+
+  device {
+    name = "eth0"
+    type = "nic"
+
+    properties = {
+      name = "eth0"
+      parent = "colosseum-wan"
+      nictype = "bridged"
+    }
+  }
+
+  device {
+    name = "game"
+    type = "nic"
+
+    properties = {
+      name = "game"
+      network = "colosseum-network"
+    }
+  }
+}
+
