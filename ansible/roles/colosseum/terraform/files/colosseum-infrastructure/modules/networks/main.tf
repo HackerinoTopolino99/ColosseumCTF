b58e62eb345540acd2080843ef336fefa263eba3
Giuliano Vallone <giuliano.vallone@ikmail.com>
2025-05-27 10:59:26 +0200

    Added network colosseum-wan

--- ansible/roles/colosseum/terraform/files/colosseum-infrastructure/modules/networks/main.tf
+++ ansible/roles/colosseum/terraform/files/colosseum-infrastructure/modules/networks/main.tf
@@ -3,6 +3,7 @@ resource "incus_network" "colosseum-network" {
   type = "ovn"
 
   config = {
+    "bridge.mtu" = "1300"
     "ipv4.address" = "10.254.0.2/8"
     "ipv4.nat" = "false"
     "ipv4.dhcp" = "false"
@@ -11,8 +12,42 @@ resource "incus_network" "colosseum-network" {
   }
 }
 
+resource "incus_network" "colosseum_wan" {
+  count  = length(var.nodes) == 1 ? 1 : 0
+  name = "colosseum-wan"
+  type = "bridge"
+
+  config = {
+    "ipv4.address" = "192.168.45.1/23"
+    "ipv4.nat" = "true"
+    "ipv4.dhcp" = "true"
+    "ipv6.address" = "none"
+  }
+}
+
+resource "incus_network" "colosseum_wan_node" {
+  for_each = length(var.nodes) > 2 ? toset(var.nodes) : toset([])
+  name = "colosseum-wan"
+  type = "bridge"
+  target = each.value
+}
+
+resource "incus_network" "colosseum_wan_cluster" {
+  count  = length(var.nodes) > 1 ? 1 : 0
+  name = "colosseum-wan"
+  type = "bridge"
+
+  config = {
+    "ipv4.address" = "192.168.45.1/23"
+    "ipv4.nat" = "true"
+    "ipv4.dhcp" = "true"
+    "ipv6.address" = "none"
+  }
+  depends_on = [incus_network.colosseum_wan_node]
+}
+
 resource "incus_network_forward" "colosseum-vpn-forward" {
-  network = "incusbr0"
+  network = "colosseum-wan"
   listen_address = var.cluster_address
 
   ports = concat(
@@ -21,9 +56,14 @@ resource "incus_network_forward" "colosseum-vpn-forward" {
         description = "Wireguard Forward"
         protocol = "udp"
         listen_port = "51820"
-        target_address = "172.16.0.2"
+        target_address = "192.168.45.2"
         target_port = "51820"
       }
     ],
   )
+
+  depends_on = [
+    incus_network.colosseum_wan,
+    incus_network.colosseum_wan_cluster
+  ]
 }
--- ansible/roles/colosseum/terraform/files/colosseum-infrastructure/modules/networks/variables.tf
+++ ansible/roles/colosseum/terraform/files/colosseum-infrastructure/modules/networks/variables.tf
@@ -2,3 +2,7 @@ variable "cluster_address" {
   type = string
 }
 
+variable "nodes" {
+  description = "List of nodes of the cluster"
+  type = list(string)
+}
