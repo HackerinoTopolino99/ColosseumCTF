e8cf2ecb135a47e22eac859a806d76971c15fb2e
Iaio99 <giuliano.vallone@ikmail.com>
2025-06-01 08:53:40 +0200

    Added hwaddress to the vulnbox and gameserver on colosseum-network and static addresses on colosseum-wan

--- ansible/roles/colosseum/terraform/files/colosseum-infrastructure/modules/instances/main.tf
+++ ansible/roles/colosseum/terraform/files/colosseum-infrastructure/modules/instances/main.tf
@@ -7,6 +7,24 @@ resource "incus_instance" "gameserver" {
   config = {
     "boot.autostart" = true
   }
+
+  device {
+    name = "game"
+    type = "nic"
+    properties = {
+      network = colosseum-network
+      hwadrr = "02:12:99:00:00:00"
+    }
+  }
+
+  device {
+    name = "eth0"
+    type = "nic"
+    properties = {
+      network = "colosseum-wan"
+      "ipv4.address" = "192.168.44.3"
+    }
+  }
 }
 
 resource "incus_instance" "vulnbox" {
@@ -22,6 +40,24 @@ resource "incus_instance" "vulnbox" {
     "security.nesting" = true
     "security.syscalls.intercept.mknod" = true
     "security.syscalls.intercept.setxattr" = true
+  }
+
+  device {
+    name = "game"
+    type = "nic"
+    properties = {
+      network = colosseum-network
+      hwadrr = "12:15:99:00:00:${format("%x", count.index)}"
+    }
+  }
+
+  device {
+    name = "eth0"
+    type = "nic"
+    properties = {
+      network = "colosseum-wan"
+      "ipv4.address" = "192.168.45.${count.index + 1}"
+    }
   }
 }
 
@@ -43,4 +79,4 @@ resource "incus_instance" "router" {
       "ipv4.address" = "192.168.44.2"
     }
   }
-} 
+}
