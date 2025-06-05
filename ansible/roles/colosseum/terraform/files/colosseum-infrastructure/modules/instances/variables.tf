e318ed29ffc47491e735451ed29493790c46e8c3
Giuliano Vallone <giuliano.vallone@ikmail.com>
2025-06-05 19:01:01 +0200

    Refactoring

--- ansible/roles/colosseum/terraform/files/colosseum-infrastructure/modules/instances/variables.tf
+++ ansible/roles/colosseum/terraform/files/colosseum-infrastructure/modules/instances/variables.tf
@@ -1,14 +1,14 @@
 variable "teams" {
+  type        = list(string)
   description = "List of teams that will partecipate"
-  type = list(string)
 }
 
 variable "instance_type" {
-  type = string
+  type    = string
   default = "container"
 
   validation {
-    condition = contains(["container", "virtual-machine"], var.instance_type)
+    condition     = contains(["container", "virtual-machine"], var.instance_type)
     error_message = "The value must be container or virtual-machine"
   }
 }
